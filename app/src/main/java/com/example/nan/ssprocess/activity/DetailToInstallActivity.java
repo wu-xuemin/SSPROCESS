f4dde344e27b7a58b23dd8d2844ebcf1e9d05668
Johnny <740602761@qq.com>
2017-12-04 22:51:04 +0800

    detail界面

---
+++ app/src/main/java/com/example/nan/ssprocess/activity/DetailToInstallActivity.java
@@ -0,0 +1,15 @@
+package com.example.nan.ssprocess.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.nan.ssprocess.R;
+
+public class DetailToInstallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_to_install);
+    }
+}
