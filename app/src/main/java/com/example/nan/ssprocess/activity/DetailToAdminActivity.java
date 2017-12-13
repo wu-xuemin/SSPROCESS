b5d88331dec30f156e239923cf4c87ccf5d1e790
nan <740602761@qq.com>
2017-12-13 17:28:28 +0800

    detail界面整理

--- app/src/main/java/com/example/nan/ssprocess/activity/DetailToAdminActivity.java
+++ app/src/main/java/com/example/nan/ssprocess/activity/DetailToAdminActivity.java
@@ -1,6 +1,8 @@
 package com.example.nan.ssprocess.activity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
@@ -12,6 +14,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.blankj.utilcode.util.ToastUtils;
@@ -20,6 +24,7 @@ import com.example.nan.ssprocess.service.MyMqttService;
 import com.example.nan.ssprocess.app.SinSimApp;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -32,16 +37,19 @@ public class DetailToAdminActivity extends AppCompatActivity {
     private static final String TAG="nlgDetailToAdmin";
     private String mFilePath = null;// 获取SD卡路径
     private static final int START_CAMERA = 10001;
+    private ImageView takePhotosImageView;
+    private Uri photoUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_to_admin);
 
-        ImageButton takePhotosImageButton=findViewById(R.id.take_photos_ib);
         Button publishButton = findViewById(R.id.publish_button);
+        takePhotosImageView=findViewById(R.id.take_photos_iv);
 
-        takePhotosImageButton.setOnClickListener(new View.OnClickListener() {
+
+        takePhotosImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openCamera();
@@ -55,16 +63,14 @@ public class DetailToAdminActivity extends AppCompatActivity {
         });
 
         //保存拍照文件的路径
-        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/CRH");
+        File file = new File(Environment.getExternalStorageDirectory().getPath() + "/SINSIM");
         Log.d(TAG, "onCreate: 1"+file);
         if(!file.exists()){
             if(file.mkdir()){
                 mFilePath = file.getAbsolutePath();
-                Log.d(TAG, "onCreate: 2"+mFilePath);
             }
         }else {
             mFilePath = file.getAbsolutePath();
-            Log.d(TAG, "onCreate: 3"+mFilePath);
         }
     }
 
@@ -85,12 +91,12 @@ public class DetailToAdminActivity extends AppCompatActivity {
 
         file.setWritable(true);
 
-        Uri photoUri;
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             photoUri = FileProvider.getUriForFile(this.getApplicationContext(), "com.example.nan.ssprocess.fileprovider", file);
         } else {
             photoUri = Uri.fromFile(file); // 传递路径
         }
+        Log.d(TAG, "openCamera: url: "+photoUri);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);// 更改系统默认存储路径
         startActivityForResult(intent, START_CAMERA);
     }
@@ -101,15 +107,14 @@ public class DetailToAdminActivity extends AppCompatActivity {
         if (resultCode == RESULT_OK) { // 如果返回数据
             if (requestCode == START_CAMERA) { // 判断请求码是否为REQUEST_CAMERA,如果是代表是这个页面传过去的，需要进行获取
                 Toast.makeText(this, "已保存至以下目录：" + mFilePath, Toast.LENGTH_SHORT).show();
-                String taskKey = SinSimApp.getApp().getCache().getString("current_task");
-                if(taskKey != null) {
-                    int photoTimes = 0;
-                    if(SinSimApp.getApp().getCache().getString(taskKey) != null) {
-                        photoTimes = Integer.valueOf(SinSimApp.getApp().getCache().getString(taskKey));
-                    }
-                    SinSimApp.getApp().getCache().put(taskKey, String.valueOf(++photoTimes), 60*10);//拍照有限间隔是10分钟
-                } else {
-                    ToastUtils.showLong("如需拍照卡控，请先确认对应的作业内容后再进行拍照！");
+                Bitmap bitmap = null;
+                try {
+                    //在imageView中显示
+                    bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(photoUri));
+                    takePhotosImageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    Log.d(TAG, "onActivityResult: error:"+e);
+                    e.printStackTrace();
                 }
             }
         } else {
