59b28df99169c20aedc0f513dbbe5ea94307403c
Hu Tong <htbasketball@163.com>
2018-04-05 01:40:57 +0800

    遇到扫码不成功的情况，所以目前设置了20秒扫码不成功，则跳出手动输入框供填写机器编码

--- app/src/main/java/com/example/nan/ssprocess/ui/activity/ScanQrcodeActivity.java
+++ app/src/main/java/com/example/nan/ssprocess/ui/activity/ScanQrcodeActivity.java
@@ -8,8 +8,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.example.nan.ssprocess.R;
+
+import java.util.Timer;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import cn.bingoogolapple.qrcode.core.QRCodeView;
 import cn.bingoogolapple.qrcode.zxing.ZXingView;
@@ -22,6 +30,8 @@ public class ScanQrcodeActivity extends AppCompatActivity implements QRCodeView.
     private static final String TAG = "nlgScanQrcodeActivity";
     private AlertDialog scanQrResultDialog;
     private ZXingView mQRCodeView;
+    private ScheduledExecutorService mStopScanTimer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +55,21 @@ public class ScanQrcodeActivity extends AppCompatActivity implements QRCodeView.
         mQRCodeView.showScanRect();
         Log.d(TAG, "onStart: startCamera");
         mQRCodeView.startSpot();
-
+        //org.apache.commons.lang3.concurrent.BasicThreadFactory
+        mStopScanTimer = new ScheduledThreadPoolExecutor(1);
+        mStopScanTimer.schedule(new Runnable() {
+            @Override
+            public void run() {
+                ToastUtils.showShort("扫描失败，切换至手动模式！");
+                mQRCodeView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mQRCodeView.stopSpot();
+                        showDialog(null);
+                    }
+                });
+            }
+        }, 20, TimeUnit.SECONDS);
     }
 
     @Override
@@ -61,6 +85,9 @@ public class ScanQrcodeActivity extends AppCompatActivity implements QRCodeView.
         if (scanQrResultDialog!=null){
             scanQrResultDialog.dismiss();
         }
+        if(!mStopScanTimer.isShutdown() ) {
+            mStopScanTimer.shutdownNow();
+        }
     }
     
     private void vibrate() {
@@ -72,33 +99,7 @@ public class ScanQrcodeActivity extends AppCompatActivity implements QRCodeView.
 
     @Override
     public void onScanQRCodeSuccess(final String result) {
-        Log.d(TAG, "result:" + result);
-        scanQrResultDialog = new AlertDialog.Builder(ScanQrcodeActivity.this).create();
-        scanQrResultDialog.setTitle("扫描结果");
-        scanQrResultDialog.setMessage("扫描结果： "+result);
-        scanQrResultDialog.setButton(AlertDialog.BUTTON_POSITIVE, "确定",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //根据result获取对应taskRecordDetail
-                        Intent intent = getIntent();
-                        intent.putExtra("mMachineNamePlate", result);
-                        ScanQrcodeActivity.this.setResult(RESULT_OK, intent);
-                        ScanQrcodeActivity.this.finish();
-                    }
-                });
-        scanQrResultDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"重新扫描",
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        //重新扫描
-                        mQRCodeView.startSpot();
-                    }
-                });
-        // 显示
-        scanQrResultDialog.show();
-        //震动
-        vibrate();
+        showDialog(result);
     }
 
     @Override
@@ -115,5 +116,66 @@ public class ScanQrcodeActivity extends AppCompatActivity implements QRCodeView.
             default:
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialog(final String result) {
+        Log.d(TAG, "result:" + result);
+        scanQrResultDialog = new AlertDialog.Builder(ScanQrcodeActivity.this).create();
+        scanQrResultDialog.setTitle("扫描结果");
+        final EditText et = new EditText(this);
+        if(result != null) {
+            scanQrResultDialog.setMessage("扫描结果： "+result);
+        } else {
+            scanQrResultDialog.setView(et);
+        }
+        scanQrResultDialog.setButton(AlertDialog.BUTTON_POSITIVE, "确定",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String machineIDStr = "";
+                        if(result != null) {
+                            machineIDStr = result;
+                        } else {
+                            machineIDStr = et.getText().toString();
+                        }
+                        if("".equals(machineIDStr)) {
+                            ToastUtils.showShort("机器编号不能为空！");
+                        } else {
+                            //根据result获取对应taskRecordDetail
+                            Intent intent = getIntent();
+                            intent.putExtra("mMachineNamePlate", machineIDStr);
+                            ScanQrcodeActivity.this.setResult(RESULT_OK, intent);
+                            if(!mStopScanTimer.isShutdown() ) {
+                                mStopScanTimer.shutdownNow();
+                            }
+                            ScanQrcodeActivity.this.finish();
+                        }
+                    }
+                });
+        scanQrResultDialog.setButton(AlertDialog.BUTTON_NEGATIVE,"重新扫描",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //重新扫描
+                        mQRCodeView.startSpot();
+                        mStopScanTimer.schedule(new Runnable() {
+                            @Override
+                            public void run() {
+                                ToastUtils.showShort("扫描失败，切换至手动模式！");
+                                mQRCodeView.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        mQRCodeView.stopSpot();
+                                        showDialog(null);
+                                    }
+                                });
+                            }
+                        }, 20, TimeUnit.SECONDS);
+                    }
+                });
+        // 显示
+        scanQrResultDialog.show();
+        //震动
+        vibrate();
     }
 }
